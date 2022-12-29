7e23d2c22c3fd488f60d81c604af89566e2d89a6
Henro-guy <henryachugbu@gmail.com>
2022-12-28 20:01:07 -0800

    task 5 a program that prints single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints single numbers of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+	printf("%d", i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
