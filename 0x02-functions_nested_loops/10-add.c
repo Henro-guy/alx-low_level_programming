f27a71c357862a6240019b794a895c5960def96b
Henro-guy <henryachugbu@gmail.com>
2023-01-04 11:54:01 -0800

    task 10 a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers
+ * and returns the result
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ *
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
